3641bde0fa9cc7016a723a4a4889bdd3c4975c22
ESP32 - ESP8266 para Tec.Dig. UA <125473983+jtoobe@users.noreply.github.com>
2024-10-02 16:05:15 -0300

    Update ESP32_MACaddress.ino

--- ESP32_MACaddress.ino
+++ ESP32_MACaddress.ino
@@ -1,5 +1,3 @@
-// para ESP8266
-//#include <ESP8266WiFi.h>
 // para ESP32
 #include <WiFi.h>
 
@@ -7,11 +5,12 @@ void setup(){
 
    Serial.begin(115200);
 
-   // espera de 5 segundos (para ESP01)
+   // espera de 2 segundos (para ESP01)
    delay(2000);
 
    // para ESP32
    WiFi.begin();
+   
 
    Serial.println();
    Serial.print("MAC: ");
