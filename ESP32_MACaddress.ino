c7da2118f3791e949f99b68e2b8c3d0c8cbb14e6
ESP32 - ESP8266 para Tec.Dig. UA <125473983+jtoobe@users.noreply.github.com>
2025-10-15 10:39:00 -0300

    Update ESP32_MACaddress.ino

--- ESP32_MACaddress.ino
+++ ESP32_MACaddress.ino
@@ -1,21 +1,32 @@
 // para ESP32
+
+// Muestra la direccion macaddress en un formato listo para copiar y pegar para su uso con ESP-NOW
+
+
 #include <WiFi.h>
 
-void setup(){
+void setup() {
+  Serial.begin(115200);
+  delay(2000);  // espera de 2 segundos (para ESP01)
 
-   Serial.begin(115200);
+  // Inicializa WiFi (no se conecta a ninguna red)
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
 
-   // espera de 2 segundos (para ESP01)
-   delay(2000);
+  // Obtiene la dirección MAC como array de bytes
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
 
-   // para ESP32
-   WiFi.begin();
-   
-
-   Serial.println();
-   Serial.print("MAC: ");
-   Serial.println(WiFi.macAddress());
-
+  // Muestra la MAC en formato 0xXX,0xXX,0xXX,0xXX,0xXX,0xXX
+  Serial.println();
+  Serial.print("MAC: ");
+  for (int i = 0; i < 6; i++) {
+    Serial.print("0x");
+    if (mac[i] < 0x10) Serial.print("0"); // agrega un 0 delante si es menor a 0x10
+    Serial.print(mac[i], HEX);
+    if (i < 5) Serial.print(",");
+  }
+  Serial.println();
 }
 
-void loop(){}
+void loop() {}
